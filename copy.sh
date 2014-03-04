d74a9c1f544ad2eb8c5ef096375b75f99ef4b0cd
tolia <toliademidov@gmail.com>
2014-03-04 12:04:45 +0400

    update

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
-cp -r angular.js/build/angular.js clipped.angular.js
-cp -r angular.js/src/ngLocale/angular-locale_ru.js angular-locale_ru.js
+cp -r angular/build/angular.js clipped.angular.js
+cp -r angular/src/ngLocale/angular-locale_ru.js angular-locale_ru.js
